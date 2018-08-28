52e65ad907cdeaca8ae49169909391674065a2b2
grigoruk <anna.v.grigoruk@gmail.com>
2018-08-28 20:57:48 +0400

    Stepic tasks. Part 3.

--- stepic_3_6.py
+++ stepic_3_6.py
@@ -4,9 +4,5 @@ with open(file, 'r') as fr:
     f_name_to_get = fr.readline().strip()
 
 f_get = requests.get(f_name_to_get)
-cnt = 0
-print(f_get.text)
-with open (f_get, 'r') as fr:
-    while fr.readline():
-        cnt += 1
+cnt = len(f_get.text.splitlines())
 print(cnt)
